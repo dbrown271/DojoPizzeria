1880e566a2d5932b6b0fba8cfda7080349925e00
dbrown271 <d.brown271@icloud.com>
2021-09-21 16:03:30 -0500

    initial commit

---
+++ dojoPizza.js
@@ -0,0 +1,64 @@
+//Pizza Number 1
+var pizzaOven = {
+    "crust":  "deep dish",
+    "sauce":  "traditional",
+    "cheeses":  ["mozzarella"],
+    "toppings":  ["pepperoni", "sausage"],
+    "pizzaOvenInfo": function() {
+        console.log("Crust: " + pizzaOven.crust);
+        console.log("Sauce: " + pizzaOven.sauce);
+        console.log("Cheeses: " + pizzaOven.cheeses);
+        console.log("Toppings: " + pizzaOven.toppings)
+    }
+}
+
+pizzaOven.pizzaOvenInfo()
+
+//Pizza Number 2
+var pizzaOven = {
+    "crust":  "hand tossed",
+    "sauce":  "marinara",
+    "cheeses":  ["mozzarella", "feta"],
+    "toppings":  ["mushrooms", "olives", "onions"],
+    "pizzaOvenInfo": function() {
+        console.log("Crust: " + pizzaOven.crust);
+        console.log("Sauce: " + pizzaOven.sauce);
+        console.log("Cheeses: " + pizzaOven.cheeses);
+        console.log("Toppings: " + pizzaOven.toppings)
+    }
+}
+
+pizzaOven.pizzaOvenInfo()
+
+//Pizza Number 3
+var pizzaOven = {
+    "crust":  "personal pan",
+    "sauce":  "marinara",
+    "cheeses":  ["extra cheese"],
+    "toppings":  ["sausage", "bacon", "pineapples"],
+    "pizzaOvenInfo": function() {
+        console.log("Crust: " + pizzaOven.crust);
+        console.log("Sauce: " + pizzaOven.sauce);
+        console.log("Cheeses: " + pizzaOven.cheeses);
+        console.log("Toppings: " + pizzaOven.toppings)
+    }
+}
+
+pizzaOven.pizzaOvenInfo()
+
+//Pizza Number 4
+var pizzaOven = {
+    "crust":  "thin crust",
+    "sauce":  "white sauce",
+    "cheeses":  ["gooda"],
+    "toppings":  "pepperoni",
+    "pizzaOvenInfo": function() {
+        console.log("Crust: " + pizzaOven.crust);
+        console.log("Sauce: " + pizzaOven.sauce);
+        console.log("Cheeses: " + pizzaOven.cheeses);
+        console.log("Toppings: " + pizzaOven.toppings)
+    }
+}
+
+pizzaOven.pizzaOvenInfo()
+
